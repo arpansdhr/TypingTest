758f37eacd5fed687da34d8d8929c06e42c20f34
Arpan Sutradhar <sendmail2arpansutradhar@gmail.com>
2024-06-03 23:36:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,106 @@
+const typingText = document.querySelector('.typing-text p');
+const input = document.querySelector('.wrapper .input-field');
+const time = document.querySelector('.time span b');
+const mistakes = document.querySelector('.mistake span');
+const wpm = document.querySelector('.wpm span');
+const cpm = document.querySelector('.cpm span');
+const btn = document.querySelector('button');
+
+let timer;
+let maxTime = 60;
+let timeLeft = maxTime;
+let charIndex = 0;
+let mistake = 0;
+let isTyping = false;
+
+function loadParagraph() {
+    const paragraph = [
+        "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+        "Avoid daydreaming about the years to come.",
+        "You are the most important person in your whole life.",
+        "Always be true to who you are, and ignore what other people have to say about you.",
+        "Only demonstrate your strength when it’s really required.",
+        "I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin but by the content of their character.",
+        "Tis better to have loved and lost than never to have loved at all."
+    ];
+
+    const randomIndex = Math.floor(Math.random() * paragraph.length);
+    typingText.innerHTML = '';
+    for (const char of paragraph[randomIndex]) {
+        console.log(char);
+        typingText.innerHTML += `<span>${char}</span>`;
+    }
+    typingText.querySelectorAll('span')[0].classList.add('active');
+    document.addEventListener('keydown', () => input.focus());
+    typingText.addEventListener("click", () => {
+        input.focus();
+    });
+}
+
+function initTyping() {
+    const char = typingText.querySelectorAll('span');
+    const typedChar = input.value.charAt(charIndex);
+    if (charIndex < char.length && timeLeft > 0) {
+
+        if (!isTyping) {
+            timer = setInterval(initTime, 1000);
+            isTyping = true;
+        }
+
+        if (char[charIndex].innerText === typedChar) {
+            char[charIndex].classList.add('correct');
+            console.log("correct");
+        } else {
+            mistake++;
+            char[charIndex].classList.add('incorrect');
+            console.log("incorrect");
+        }
+        char[charIndex].classList.remove('active');
+        charIndex++;
+        if (charIndex < char.length) {
+            char[charIndex].classList.add('active');
+        }
+
+        mistakes.innerText = mistake;
+        cpm.innerText = charIndex - mistake;
+
+        if (charIndex === char.length) {
+            clearInterval(timer);
+            input.value = '';
+            input.disabled = true;
+        }
+    } else {
+        clearInterval(timer);
+        input.value = '';
+    }
+}
+
+function initTime() {
+    if (timeLeft > 0) {
+        timeLeft--;
+        time.innerText = timeLeft;
+        let wpmVal = Math.round(((charIndex - mistake) / 5) / (maxTime - timeLeft) * 60);
+        wpm.innerText = wpmVal;
+    } else {
+        clearInterval(timer);
+    }
+}
+
+function reset() {
+    loadParagraph();
+    clearInterval(timer);
+    timeLeft = maxTime;
+    time.innerText = timeLeft;
+    input.value = '';
+    input.disabled = false;
+    charIndex = 0;
+    mistake = 0;
+    isTyping = false;
+    wpm.innerText = 0;
+    cpm.innerText = 0;
+    mistakes.innerText = 0;
+}
+
+input.addEventListener("input", initTyping);
+btn.addEventListener("click", reset);
+loadParagraph();
